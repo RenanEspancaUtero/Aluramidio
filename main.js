440b6229f3b661f0e35947ef2c0525f571cdbed5
RenanEspancaUtero <135145557+RenanEspancaUtero@users.noreply.github.com>
2023-10-18 08:15:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,6 @@
-document.querySelector('.tecla_pom');
+function tocaSomPom(){
+document.querySelector('som_tecla_pom').play('');
+}
+document.querySelector('.querySelectorAll').onclick = tocaSomPom ();
 
 document.querySelector('#som_tecla_pom').play('');

ce36fa6ef5da88c70249e1123f311c68299e4a90
RenanEspancaUtero <135145557+RenanEspancaUtero@users.noreply.github.com>
2023-10-18 12:38:57 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,22 @@
-function tocaSomPom(){
-document.querySelector('som_tecla_pom').play('');
+function tocaSomPom(idElementoAudio){
+document.querySelector(idElementoAudio).play('');
 }
-document.querySelector('.querySelectorAll').onclick = tocaSomPom ();
 
-document.querySelector('#som_tecla_pom').play('');
+const listaDeTeclas = document.querySelectorAll('.tecla');
+
+
+listaDeTeclas[0].onclick = tocaSomPom;
+
+
+for(contador = 0; contador < listaDeTeclas. length;contador++){
+    const instrumento = listaDeTecla [contador].classsList[1];
+
+    const tecla = listaDeTeclas[contador];
+
+    const idAudio = #som_${instrumento};
+    tecla.onclick = function(){
+        tocaSomPom(idAudio);
+    };
+
+}
+
